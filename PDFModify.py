c0ec710979bb02b3797d35dac4bfa4ce51671035
tvvshehri <74836451+tvvshehri@users.noreply.github.com>
2022-07-18 18:37:50 +0300

    Add files via upload

---
+++ PDFModify.py
@@ -0,0 +1,14 @@
+import PyPDF2
+
+pdf_file = open('PDFModify.pdf', 'rb')
+
+pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+page_num = pdf_reader.numPages
+page = pdf_reader.getPage(page_num-1)
+text = page.extractText()
+
+file = open(r"C:\Users\tvvsh\OneDrive\Desktop\Project\\PDFModify.txt", "a")
+file.writelines(text)
+file.close()
+
+
