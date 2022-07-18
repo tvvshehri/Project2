09f6cb592930c2a9f5cc53a2416c8204ed502363
tvvshehri <74836451+tvvshehri@users.noreply.github.com>
2022-07-18 20:58:06 +0300

    Add files via upload

---
+++ Song.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+file_path = 'Song.pdf'
+pdf = PdfFileReader(file_path)
+
+with open('Song.txt', 'w') as f:
+    for page_num in range(pdf.numPages):
+        page = pdf.getPage(page_num)
+
+        try:
+            txt= page.extractText()
+            print(' '.center(100, '-'))
+        except:
+            pass
+        else:
+            f.write('\npage{0} \n'.format(page_num+1))
+            f.write(''.center(100,'-'))
+            f.write('\n')
+            f.write(txt)
+    f.close()
