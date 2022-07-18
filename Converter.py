8727f7f96aad747d7e6ce370314df976d16039a8
tvvshehri <74836451+tvvshehri@users.noreply.github.com>
2022-07-19 02:17:57 +0300

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,23 @@
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
+def pdf2txt(PDF_File, TXT_File):
+    File= open(PDF_File, 'rb')
+    resMgr = PDFResourceManager()
+    retData = io.StringIO()
+    TxtConverter = TextConverter(resMgr, retData, laparams=LAParams())
+    interpreter= PDFPageInterpreter(resMgr, TxtConverter)
+    for page in PDFPage.get_pages(File):
+        interpreter.process_page(page)
+
+    txt = retData.getvalue()
+    with open(TXT_File, 'w') as f:
+        f.write(txt)
+
+PDF_File = 'Converter.pdf'
+TXT_File = 'Converter.txt'
+pdf2txt(PDF_File, TXT_File)
